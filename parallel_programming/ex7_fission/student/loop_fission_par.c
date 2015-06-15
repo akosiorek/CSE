0ab5ca467c69a7adaa208a82e180c188a0a6d974
Adam Kosiorek <kosiorek.adam@gmail.com>
2015-06-15 17:38:12 +0200

    boosted loop fission exercise

--- parallel_programming/ex7_fission/student/loop_fission_par.c
+++ parallel_programming/ex7_fission/student/loop_fission_par.c
@@ -19,9 +19,9 @@ void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned l
 		}
 	}
 
-	for (int j = 1; j < N; j++) {
-		#pragma omp parallel for
-		for (int i = 1; i < N; i++) {
+	#pragma omp parallel for
+	for (int i = 1; i < N; i++) {
+		for (int j = 1; j < N; j++) {
 			a[i][j]   = a[i][j] * b[i][j];
 			b[i][j + 1] = 2 * a[i][j] * c[i - 1][j];
 		}
