a3cbbb0ea5d4f59a985a78e926a351713cb8295b
Adam Kosiorek <kosiorek.adam@gmail.com>
2015-06-24 14:19:41 +0200

    game of life ex9 works

--- parallel_programming/ex9/student/gol_par.c
+++ parallel_programming/ex9/student/gol_par.c
@@ -6,6 +6,10 @@
 
 #include "helper.h"
 
+int z = 0;
+//#define LOG(x) printf("Rank: %d, i: %d, %s: %d\n", rank, z++, #x, x); 
+#define LOG(x)
+
 unsigned int gol(unsigned char *grid, unsigned int dim_x, unsigned int dim_y, unsigned int time_steps)
 {
 	// READ ME! Parallelize this function to work with MPI. It must work even with a single processor.
@@ -26,11 +30,16 @@ unsigned int gol(unsigned char *grid, unsigned int dim_x, unsigned int dim_y, un
 		np = dim_y;
 	}
 
+	int proc_prev = rank == 0 ? np - 1 : rank - 1;
+	int proc_next = (rank + 1) % np;
+	LOG(proc_prev);
+	LOG(proc_next);	
+
 	int rowsPerProc = dim_y / np;
 	int remainderRows = dim_y % np;
 
 	rowsPerProc += remainderRows > rank;
-
+	LOG(rowsPerProc);
 
 	int offset = dim_x ;
 	int size = dim_x * rowsPerProc;
@@ -47,8 +56,9 @@ unsigned int gol(unsigned char *grid, unsigned int dim_x, unsigned int dim_y, un
 		 counts = malloc(np * sizeof(*counts));
 		 displacement = malloc(np * sizeof(*displacement));
 
+		int rows = dim_y / np;
 		for(int i = 0; i < np; ++i) {
-			counts[i] = rowsPerProc + (remainderRows > 0);
+			counts[i] = rows + (remainderRows > 0);
                         counts[i] *= dim_x * sizeof(uchar);
 			--remainderRows;
 		}
@@ -57,32 +67,74 @@ unsigned int gol(unsigned char *grid, unsigned int dim_x, unsigned int dim_y, un
 		for(int i = 1; i < np; ++i) {
 			displacement[i] = displacement[i - 1] + counts[i - 1];
 		}
+
+		//for(int i = 0; i < np; ++i) {
+		//	printf("count: %d, disp: %d\n", counts[i], displacement[i]);
+		//}
 	}
 
 	MPI_Scatterv(	grid,	// send buffer
 			counts,
 			displacement,
 			MPI_CHAR,
-			grid_in + offset, // begin of true data
+			grid_in, // begin of true data
 			size,
 			MPI_CHAR,
 			0,	// root
 			MPI_COMM_WORLD);
 
 
+
+
+	MPI_Status status;
 	for (int t = 0; t < time_steps; ++t)
 	{
-		for (int y = 0; y < dim_y; ++y)
+		LOG(proc_prev);
+		LOG(proc_next);
+		MPI_Sendrecv(	grid_in,	// send buf
+				dim_x,		// send count
+				MPI_CHAR, 	// send type
+				proc_prev,	// destination
+				0,		// send tag
+				grid_in + size, //recv buff
+				dim_x,		// recv count
+				MPI_CHAR,	// recv type
+				proc_next,	// source
+				0,		// recv tag
+				MPI_COMM_WORLD,	// communicator
+				&status);	// status
+
+
+		MPI_Sendrecv(	grid_in + size - dim_x,	// send buf
+				dim_x,		// send count
+				MPI_CHAR, 	// send type
+				proc_next,	// destination
+				0,		// send tag
+				grid_in + size + offset, //recv buff
+				dim_x,		// recv count
+				MPI_CHAR,	// recv type
+				proc_prev,	// source
+				0, 		// recv tag
+				MPI_COMM_WORLD,	// communicator
+				&status);	// status
+
+
+	//printf("Rank: %d\n", rank);
+	//print_gol(grid_in, dim_x, rowsPerProc + 2);
+
+
+
+		for (int y = 0; y < rowsPerProc; ++y)
 		{
 			for (int x = 0; x < dim_x; ++x)
 			{
-				evolve(grid_in, grid_out, dim_x, dim_y, x, y);
+				evolve(grid_in, grid_out, dim_x, rowsPerProc + 2, x, y);
 			}
 		}
 		swap((void**)&grid_in, (void**)&grid_out);
 	}
 
-	MPI_Gatherv(	grid_in + offset,
+	MPI_Gatherv(	grid_in,
 			size,
 			MPI_CHAR,
 			grid,
